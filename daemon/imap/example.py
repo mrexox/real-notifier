3f70eaabb1d460c782c68890f7b73bda9610ef3b
Valentine <mrexox@yahoo.com>
2018-04-01 18:30:43 +0000

    Imap class partially

--- daemon/imap/example.py
+++ daemon/imap/example.py
@@ -2,6 +2,6 @@ import imaplib
 import getpass
 
 imap = imaplib.IMAP4_SSL('imap.mail.ru')
-print(imap.login('meexox@mail.ru', getpass.getpass()))
+print(imap.login('your@mail.ru', getpass.getpass()))
 print(imap.select('INBOX'))
 print(imap.search(None, '(UNSEEN)'))
---
+++ daemon/imap/imap.py
@@ -0,0 +1,37 @@
+import imaplib
+import getpass
+from file_notifier import FileNotifier
+
+class Imap():
+    """Mail communication class
+    
+    Not fully written yet, so needs:
+        Loading configuration
+    """
+
+    def __init__(self, login, password):
+        service = login.split('@')[1] # split -> ['name', 'service.ru']
+        if service not in ['mail.ru', 'yandex.ru']:
+            print("Not supported service")
+            exit(1)
+        self.imap = imaplib.IMAP4_SSL('imap.' + service)
+        try:
+            self.imap.login(login, password)
+        except:
+            print("Not valid credentials")
+            exit(1)
+        self.imap.select('INBOX')
+
+    def new_messages_count(self):
+        (status, messages) = self.imap.search(None, '(UNSEEN)')
+        if status == 'OK':
+            return len(messages[0].split(b' '))
+        else:
+            return -1
+    
+
+if __name__ == '__main__':
+    import getpass
+    
+    imap = Imap('your@mail.ru', getpass.getpass())
+    print(imap.new_messages_count())
