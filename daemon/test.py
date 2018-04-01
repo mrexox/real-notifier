75c6b76cc07fc9a9498297c996d58a07abe3197a
Andrey Z <dn6pr0@gmail.com>
2018-04-02 01:01:22 +0300

    test.py doesn't work

--- daemon/test.py
+++ daemon/test.py
@@ -6,5 +6,18 @@ sys.path.append("./notifier")
 import imap
 import notifier
 
+from daemon import Daemon
+from config import Config
+
 if __name__ == '__main__':
-    print(imap.__file__)
+    config = Config()
+
+    imap = Imap(config.mailLogin, config.mailPassword)
+
+    out = FileNotifier()
+
+    i = 10
+    while i:
+            out.notify(imap.new_messages_count())
+            time.sleep(config.daemonTimeout)
+            i = i - 1
